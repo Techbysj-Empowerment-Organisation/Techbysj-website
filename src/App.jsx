e2b24938ed7de21a4511f4e339643ac48921ffd2
ALAMIN JUMA MAGOTI <61662333+Alamin-Juma@users.noreply.github.com>
2023-06-03 18:12:04 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
         <img src={logo} alt="Logo" />
         <div className="left-items">
           {sections.map((section) => (
-            <a key={section.name} onClick={() => scrollToSection(section.index)}>
+            <a href=""key={section.name} onClick={() => scrollToSection(section.index)}>
               {section.name}
             </a>
           ))}
@@ -113,42 +113,42 @@ function App() {
             </div>
             <h6>Web Development</h6>
             <p>we offer comprehensive web development services to help businesses establish HashLink strong online presence and achieve their digital goals. Our team of skilled web developers utilizes the latest web technologies, frameworks, and best practices to create web solutions that are visually appealing, user-friendly, and optimized for performance</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
           <div className="grid-card">
             <div className="icon">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" class="block mx-auto w-24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M11.5 0h-7c-0.825 0-1.5 0.675-1.5 1.5v13c0 0.825 0.675 1.5 1.5 1.5h7c0.825 0 1.5-0.675 1.5-1.5v-13c0-0.825-0.675-1.5-1.5-1.5zM6 0.75h4v0.5h-4v-0.5zM8 15c-0.552 0-1-0.448-1-1s0.448-1 1-1 1 0.448 1 1-0.448 1-1 1zM12 12h-8v-10h8v10z"></path></svg>            </div>
             <h6>Mobile Development</h6>
             <p>From iOS to Android, we have the expertise to build mobile apps that deliver seamless experiences, engage users, and drive results. Whether you need HashLink native app, HashLink cross-platform solution, or HashLink mobile-optimized web app, we have the skills and creativity to bring your vision to life. Our team of experienced mobile developers harness the latest technologies and industry best practices to create mobile applications that are fast, user-friendly, and visually captivating.</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
           <div className="grid-card">
             <div className="icon">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" class="block mx-auto w-24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M4.25.3C1.9.3 0 2.2 0 4.55v14.9c0 2.35 1.9 4.25 4.25 4.25h15.5c2.35 0 4.25-1.9 4.25-4.25V4.55C24 2.2 22.1.3 19.75.3zm11.31 5.13h2.03c.05-.01.09.03.1.07v9.54c0 .18.01.38.02.6.02.21.03.41.04.58 0 .07-.03.13-.1.16-.52.22-1.07.38-1.63.48-.5.09-1.02.14-1.54.14-.74.01-1.48-.14-2.15-.45-.63-.29-1.15-.77-1.51-1.36-.37-.61-.55-1.37-.55-2.28-.01-.74.18-1.47.55-2.11.38-.65.93-1.19 1.59-1.55.7-.39 1.54-.58 2.53-.58.05 0 .12 0 .21.01s.19.01.31.02V5.54c0-.07.03-.11.1-.11zm-8.93.86h1.95c.06-.01.12.03.13.1.01.01.01.02.01.03v10.26c0 .11-.05.16-.14.16H6.62c-.09 0-.13-.05-.13-.16V6.42c0-.09.05-.13.14-.13zm8.23 4.24c-.39 0-.78.08-1.13.26-.34.17-.63.42-.85.74-.22.32-.33.75-.33 1.27-.01.35.05.7.17 1.03.1.27.25.51.45.71.19.18.42.32.68.4.27.09.55.13.83.13.15 0 .29-.01.42-.02.13.01.25-.01.36-.05v-4.4c-.09-.02-.18-.04-.27-.05-.11-.01-.22-.02-.33-.02z"></path></svg>                       </div>
             <h6>Graphic Design</h6>
             <p>Our team of talented graphic designers combines artistic flair with strategic thinking to craft designs that leave HashLink lasting impression. Whether it's creating eye-catching logos, designing engaging social media graphics, crafting compelling marketing materials, or developing captivating UI/UX designs, we strive for perfection in every pixel. With our innovative and forward-thinking approach, we transform your ideas into compelling visual experiences.</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
           <div className="grid-card">
             <div className="icon">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="block mx-auto w-24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M120.998 40.998v37.943C136.22 89.471 146 109.278 146 131.001c0 13.71-3.901 26.65-10.598 36.985 3.465 1.35 7.106 2.85 10.15 4.172l122.352-22.783 5.918 54.842-111.748 23.219c-.862 16.261-2.45 32.262-5.289 51.566h336.217V40.998zM96 88.998c-16.595 0-32.002 17.747-32.002 42.004 0 24.257 15.407 42.002 32.002 42.002 16.595 0 32.002-17.745 32.002-42.002S112.595 88.998 96 88.998zm156.096 81.629l-108.592 20.22c-14.24-5.602-4.956-3.035-21.469-8.517-7.476 5.469-16.33 8.672-26.035 8.672-8.6 0-16.53-2.523-23.428-6.9-8.59 3.564-17.655 8.09-25.736 12.654-12.992 7.338-23.722 13.211-27.838 16.033v130.213h20.004V232h17.996v263.002h30.004V326h17.996v169.002h26.004v-171.84l.154-.824c9.514-50.64 12.588-77.384 13.461-109.656l109.56-22.766zm-98.153 126.375c-.952 5.682-1.991 11.64-3.146 17.996H478v-17.996zM208 344.998c-16.595 0-32.002 17.747-32.002 42.004 0 18.198 8.67 32.73 20.01 38.855 3.599-1.662 7.482-2.706 11.68-2.851 4.633-.16 8.98.767 13.052 2.42 10.968-6.352 19.262-20.63 19.262-38.424 0-24.257-15.407-42.004-32.002-42.004zm112 0c-16.595 0-32.002 17.747-32.002 42.004 0 18.198 8.67 32.73 20.01 38.855 3.599-1.662 7.482-2.706 11.68-2.851 4.633-.16 8.98.767 13.052 2.42 10.968-6.352 19.262-20.63 19.262-38.424 0-24.257-15.407-42.004-32.002-42.004zm112 0c-16.595 0-32.002 17.747-32.002 42.004 0 18.198 8.67 32.73 20.01 38.855 3.599-1.662 7.482-2.706 11.68-2.851 4.633-.16 8.98.767 13.052 2.42 10.968-6.352 19.262-20.63 19.262-38.424 0-24.257-15.407-42.004-32.002-42.004zm-223.688 95.996c-3.844.133-8.907 2.93-14.3 8.785-5.394 5.855-10.696 14.25-15.125 22.76-4.226 8.12-7.609 16.16-10.06 22.463h85.339c-3.04-6.436-7.138-14.549-12.133-22.711-5.298-8.658-11.511-17.138-17.668-22.957-6.157-5.819-11.8-8.487-16.053-8.34zm112 0c-3.844.133-8.907 2.93-14.3 8.785-5.394 5.855-10.696 14.25-15.125 22.76-4.226 8.12-7.609 16.16-10.06 22.463h85.339c-3.04-6.436-7.138-14.549-12.133-22.711-5.298-8.658-11.511-17.138-17.668-22.957-6.157-5.819-11.8-8.487-16.052-8.34zm112 0c-3.844.133-8.907 2.93-14.3 8.785-5.394 5.855-10.696 14.25-15.125 22.76-4.226 8.12-7.609 16.16-10.06 22.463h85.339c-3.04-6.436-7.138-14.549-12.133-22.711-5.298-8.658-11.511-17.138-17.668-22.957-6.157-5.819-11.8-8.487-16.052-8.34z"></path></svg>            </div>
             <h6>Training</h6>
             <p>At Nolojia, we believe in empowering individuals with the skills and knowledge they need to succeed in the ever-evolving world of technology. Our comprehensive training programs are designed to equip you with the tools and expertise you need to thrive in the digital age. Whether you want to learn software engineering, master graphic design, or gain HashLink deep understanding of data analytics, our experienced instructors will guide you every step of the way. Join Us Today!</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
           <div className="grid-card">
             <div className="icon">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="block mx-auto w-24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M21.41 11.41l-8.83-8.83c-.37-.37-.88-.58-1.41-.58H4c-1.1 0-2 .9-2 2v7.17c0 .53.21 1.04.59 1.41l8.83 8.83c.78.78 2.05.78 2.83 0l7.17-7.17c.78-.78.78-2.04-.01-2.83zM6.5 8C5.67 8 5 7.33 5 6.5S5.67 5 6.5 5 8 5.67 8 6.5 7.33 8 6.5 8z"></path></svg>            </div>
             <h6>Digital marketing</h6>
             <p>Are you ready to take your online presence to the next level? Our tech company offers cutting-edge digital marketing solutions that drive results. From search engine optimization (SEO) and pay-per-click (PPC) advertising to social media management and content marketing, our team of experienced digital marketers knows how to make your brand stand out in the crowded digital landscape.</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
           <div className="grid-card">
             <div className="icon">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="block mx-auto w-24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-5h2v5zm4 0h-2v-3h2v3zm0-5h-2v-2h2v2zm4 5h-2V7h2v10z"></path></svg>            </div>
             <h6>Data analytics</h6>
             <p>Data analytics is more than just crunching numbers. It's about turning data into insights and using those insights to make informed decisions that drive your business forward. Our team of experts uses cutting-edge tools and techniques to help you harness the power of your data, identify patterns and trends, and make data-driven decisions that can have HashLink real impact on your bottom line.</p>
-            <a className="btn-services">Learn More</a>
+            <a href=""className="btn-services">Learn More</a>
           </div>
         </div>
 
@@ -166,7 +166,7 @@ function App() {
             </span>
             <h1>Data Science</h1>
             <p>Learn Data Science using python. The course entails in-depth training on Nympy, Pandas, Matplotlib, Searborn, Plotly and many more modules. Enroll today.</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -176,7 +176,7 @@ function App() {
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg>            </span>
             <h1>Web development</h1>
             <p>Are your ready to be a web developer? Get started on this course and learn how to develop responsive websites using HTML, CSS and JavaScript. Enroll today</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -186,7 +186,7 @@ function App() {
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M4.25.3C1.9.3 0 2.2 0 4.55v14.9c0 2.35 1.9 4.25 4.25 4.25h15.5c2.35 0 4.25-1.9 4.25-4.25V4.55C24 2.2 22.1.3 19.75.3zm11.31 5.13h2.03c.05-.01.09.03.1.07v9.54c0 .18.01.38.02.6.02.21.03.41.04.58 0 .07-.03.13-.1.16-.52.22-1.07.38-1.63.48-.5.09-1.02.14-1.54.14-.74.01-1.48-.14-2.15-.45-.63-.29-1.15-.77-1.51-1.36-.37-.61-.55-1.37-.55-2.28-.01-.74.18-1.47.55-2.11.38-.65.93-1.19 1.59-1.55.7-.39 1.54-.58 2.53-.58.05 0 .12 0 .21.01s.19.01.31.02V5.54c0-.07.03-.11.1-.11zm-8.93.86h1.95c.06-.01.12.03.13.1.01.01.01.02.01.03v10.26c0 .11-.05.16-.14.16H6.62c-.09 0-.13-.05-.13-.16V6.42c0-.09.05-.13.14-.13zm8.23 4.24c-.39 0-.78.08-1.13.26-.34.17-.63.42-.85.74-.22.32-.33.75-.33 1.27-.01.35.05.7.17 1.03.1.27.25.51.45.71.19.18.42.32.68.4.27.09.55.13.83.13.15 0 .29-.01.42-.02.13.01.25-.01.36-.05v-4.4c-.09-.02-.18-.04-.27-.05-.11-.01-.22-.02-.33-.02z"></path></svg>            </span>
             <h1>Graphic design</h1>
             <p>Learn graphics designing using Adobe Photoshop, Adobe Illustrator and Corel draw. Enroll today.</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -196,7 +196,7 @@ function App() {
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M24 13.2v-6l-6-3.6-6 3.6-6-3.6-6 3.6v6l12 7.2zM8.4 10.8H6v2.4H4.8v-2.4H2.4V9.6h2.4V7.2H6v2.4h2.4zm7.2 2.4a1.2 1.2 0 01-1.2-1.2c0-.66.54-1.2 1.2-1.2.66 0 1.2.54 1.2 1.2 0 .66-.54 1.2-1.2 1.2zm3.6-2.4A1.2 1.2 0 0118 9.6c0-.66.54-1.2 1.2-1.2.66 0 1.2.54 1.2 1.2 0 .66-.54 1.2-1.2 1.2Z"></path></svg>            </span>
             <h1>Game Development</h1>
             <p>Do you have a desire to create your own game? Learn gaming by creating your own game logics and graphics. Enroll today.</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -206,7 +206,7 @@ function App() {
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M16.146 0v24l6.134-4.886V3.334zM13.293 18.758l-4.939 2.157V2.086l4.939 1.462zM1.721 18.205l3.78-.999V5.188l-3.762-.606z"></path></svg>                    </span>
             <h1>Digital Marketing</h1>
             <p>Do you want to manage businesses through marketting digitally? Our digital marketting course will give you the knowledge and skill needed to market online.</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -216,7 +216,7 @@ function App() {
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5ZM3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.58 26.58 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.933.933 0 0 1-.765.935c-.845.147-2.34.346-4.235.346-1.895 0-3.39-.2-4.235-.346A.933.933 0 0 1 3 9.219V8.062Zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a24.767 24.767 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25.286 25.286 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135Z"></path><path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2V1.866ZM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5Z"></path></svg>            </span>
             <h1>Robotics</h1>
             <p>Do you want to learn robotics? We help you learn how to code and build your first robots for successful careers in hi-tech fields.</p>
-            <a className="btn-courses">
+            <a href=""className="btn-courses">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </a>
@@ -306,10 +306,12 @@ function App() {
         </div>
         <div className="grid">
           <h3>Contact Us</h3>
-          <p>Email: info@example.com</p>
-          <p>Phone: 123-456-7890</p>
+          <p>Email: <a href="mailto:info@example.com">info@example.com</a>
+          </p>
+          <p>Phone: <a href="tel:+1234567890">123-456-7890</a>
+          </p>
         </div>
-        
+
       </footer>
 
     </div>
